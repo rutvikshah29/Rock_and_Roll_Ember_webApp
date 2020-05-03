c9827727c33e30fe01c28a4a3e17456a852f6792
Rutvik Shah <rushah@ciena.com>
2020-05-03 00:03:02 -0400

    Added star components for song ratings (modifiable ratings)

---
+++ app/components/star-rating.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+    // tagName: 'div',    div is the default tagName. hence, don't really need this.
+    classNames: ['rating-panel'],
+
+    rating: 0,
+    maxRating: 5,
+
+    stars: computed ('rating', 'maxRating' , function() {
+        let stars = [];
+        for (let i=1; i <= this.maxRating; i++) {
+            stars.push({rating: i, isFull: this.rating >= i });
+        }
+        return stars;
+    }),
+
+    actions: {
+        setRating(newRating) {
+            this.item.set('rating', newRating);
+        }
+    }
+});
